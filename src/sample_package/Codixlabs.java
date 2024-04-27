7544c3b19a1fe72f07e992539d62f7d68754036c
prashant-ingle04 <prashantingle0406@gmail.com>
2024-04-27 18:43:14 +0530

    print statement

--- src/sample_package/Codixlabs.java
+++ src/sample_package/Codixlabs.java
@@ -4,6 +4,8 @@ public class Codixlabs {
 	
 	public static void main(String[] args) {
 		System.out.println("from codixlabs we are learning");
+		
+		System.out.println("I am from MCA student");
 	}
 
 }
